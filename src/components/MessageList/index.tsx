2d825435f5b7603f949432ca3345dc728f542048
Daniel <daniel.pin@outlook.com>
2021-10-27 21:24:42 -0300

    Componente MessageList

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,5 +1,61 @@
+import styles from "./styles.module.scss";
+
+import logoImg from "../../assets/logo.svg";
+
 export function MessageList() {
-    return(
-        <h1>MessageList</h1>
-    )    
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamooo pra cima! ????
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="http://github.com/DanielPin.png"
+                alt="Daniel Pinheiro"
+              ></img>
+            </div>
+            <span>Daniel Pinheiro</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamooo pra cima! ????
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="http://github.com/DanielPin.png"
+                alt="Daniel Pinheiro"
+              ></img>
+            </div>
+            <span>Daniel Pinheiro</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamooo pra cima! ????
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="http://github.com/DanielPin.png"
+                alt="Daniel Pinheiro"
+              ></img>
+            </div>
+            <span>Daniel Pinheiro</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
